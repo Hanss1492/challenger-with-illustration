9fc78e3d32d5d794fd17ded69dbe4fefc36787dd
Hanss <hnm@agroclimatica.com>
2023-12-22 13:50:47 -0600

    agregando material icons agregando estilos a task component agregando redux y actions para agregar y eliminar

--- src/components/tasks/Tasks.tsx
+++ src/components/tasks/Tasks.tsx
@@ -1,45 +1,79 @@
-// Tasks.tsx
-
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../redux/store';
-import { useTaskController } from './TaskController';
-import {useTaskStyles}  from './Tasks.styles';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { useTaskController } from "./TaskController";
+import {
+  TextField,
+  Button,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const Tasks = () => {
-  const [newDescription, setNewDescription] = useState('');
+  const [newDescription, setNewDescription] = useState("");
   const tasks = useSelector((state: RootState) => state.tasks);
-  const classes = useTaskStyles(); // estilos
-
-  const { addTask, deleteTask } = useTaskController(); // controlador
+  const taskCount = tasks.length; // Contador de tareas
+  const { addTask, deleteTask } = useTaskController();
 
   const handleAddTask = () => {
     addTask(newDescription);
-    setNewDescription('');
+    setNewDescription("");
   };
 
   return (
-    <div className={classes.root}>
-      <h2>Tasks</h2>
-      <ul className={classes.taskList}>
+    <Paper
+      style={{
+        maxWidth: 500,
+        margin: "auto",
+        padding: 20,
+        border: "1px solid #ccc",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        Tareas ({taskCount})
+      </Typography>
+      <List>
         {tasks.map((task, index) => (
-          <li key={index} className={classes.taskItem}>
-            <span className={classes.taskDescription}>{task.description}</span>
-            <button onClick={() => deleteTask(index)}>Eliminar</button>
-          </li>
+          <ListItem key={index}>
+            <ListItemText primary={task.description} />
+            <ListItemSecondaryAction>
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => deleteTask(index)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
         ))}
-      </ul>
-      <input
-        type="text"
-        value={newDescription}
-        placeholder="Descripción de la tarea"
-        onChange={(e) => setNewDescription(e.target.value)}
-        className={classes.input}
-      />
-      <button onClick={handleAddTask} className={classes.addButton}>
-        Agregar
-      </button>
-    </div>
+      </List>
+      <div style={{ display: "flex", marginBottom: 10 }}>
+        <TextField
+          label="Descripción de la tarea"
+          variant="outlined"
+          value={newDescription}
+          onChange={(e) => setNewDescription(e.target.value)}
+          style={{ flex: 1, marginRight: 10 }}
+        />
+        <Button
+          variant="contained"
+          onClick={handleAddTask}
+          endIcon={<AddIcon />}
+          style={{ width: 120 }}
+        >
+          Agregar
+        </Button>
+      </div>
+    </Paper>
   );
 };
 
